3a6639cb1d339875929645fd04b25f9b234337fd
AnotherRizz <risqijapana55368@gmail.com>
2024-03-07 13:54:50 +0700

    hari pertama

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        prime: 'rgb(45 212 191);', // Warna utama
+        txt: ' rgb(251 191 36);', 
+        // Warna sekunder
+      },
+    },
+  },
+  plugins: [],
+}
+
