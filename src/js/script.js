2a7d22bc896ea3310983e5efd988441ccd153d24
AnotherRizz <risqijapana55368@gmail.com>
2024-03-09 12:30:54 +0700

    fitur penghitungan berat badan

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+document.getElementById("count").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var beratbadan = parseFloat(document.getElementById("bb").value);
+  var tinggibadan = parseFloat(document.getElementById("tb").value / 100);
+
+  var idm = beratbadan / (tinggibadan * tinggibadan);
+
+  document.getElementById("hasil").textContent = idm.toFixed(2);
+  
+  var kurusDiv = document.getElementById('kurus');
+  var normalDiv = document.getElementById('normal');
+  var gemukDiv = document.getElementById('gemuk');
+
+  
+  if (idm <= 17) {
+    kurusDiv.classList.remove('hidden');
+    normalDiv.classList.add('hidden');
+    gemukDiv.classList.add('hidden');
+  } else if (idm > 17 && idm <= 25) {
+    kurusDiv.classList.add('hidden');
+    normalDiv.classList.remove('hidden');
+    gemukDiv.classList.add('hidden');
+  } else {
+    kurusDiv.classList.add('hidden');
+    normalDiv.classList.add('hidden');
+    gemukDiv.classList.remove('hidden');
+  }
+});
